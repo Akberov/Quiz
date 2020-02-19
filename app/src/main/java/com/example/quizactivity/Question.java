21f2ae7bfb6092891b4587f326234f1548447d19
Renat Akberov <rentatnt@gmail.com>
2020-02-19 23:26:06 +0300

    4th chapter

---
+++ app/src/main/java/com/example/quizactivity/Question.java
@@ -0,0 +1,28 @@
+package com.example.quizactivity;
+
+public class Question {
+
+    private int mTextResID;
+    private boolean mAnswerTrue;
+
+    public Question(int mTextResID, boolean mAnswerTrue) {
+        this.mTextResID = mTextResID;
+        this.mAnswerTrue = mAnswerTrue;
+    }
+
+    public int getTextResID() {
+        return mTextResID;
+    }
+
+    public void setTextResID(int textResID) {
+        mTextResID = textResID;
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+    }
+}
